9875be2687e06c50f0de1be11d7cc7c171604e63
unknown <goldenderrick95@gmail.com>
2023-09-16 14:22:35 +0300

    solved connection

--- server/dbService.js
+++ server/dbService.js
@@ -1,5 +1,5 @@
 
-const mysql = require('mysql')
+const mysql = require('mysql2')
 require('dotenv').config()
 
 const connection = mysql.createConnection({
@@ -14,6 +14,19 @@ connection.connect((err) => {
     if(err){
         console.log(err.message)
     }else{
-        console.log('db', connection.state)
+        console.log('db', connection.sqlMessage)
     }
 })
+
+// const pool = mysql.createPool({
+//     host: process.env.envHOST,
+//     user: process.env.envUSER,
+//     password: process.env.envPASSWORD,
+//     database: process.env.envDATABASE,
+// }).promise()
+
+// const results = async () =>{
+//     const [result] = await pool.query("SELECT * FROM notes")
+//     console.log(result)
+// }
+// results()
