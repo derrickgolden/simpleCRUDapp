76ba04cb6428df4eae1975ed6a32821e8a8cf3c2
unknown <goldenderrick95@gmail.com>
2023-09-17 23:53:11 +0300

    added get and insert

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ const dotenv = require('dotenv');
 
 const dbservices = require('./dbService')
 
+const {getNote,
+    getNotes,
+    insertRow,} = require('./dbService')
+
 const app = express()
 dotenv.config()
 
@@ -11,11 +15,28 @@ app.use(cors())
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-app.get("/getAll", (req, res) =>{
-    console.log('test')
-    res.status(200).json({ success: true,
-        data: [{id: 1, name: "Derrick", date_added: "October 13, 2014 11:13:00"}]})
+app.post('/insert', async (req, res) =>{
+    const { name } = req.body
+    
+    const id = await insertRow(name)
+    console.log("id", id)
+    const nameDetails = await getNote(id);
+    console.log("details", nameDetails)
+    res.status(200).json({success: true,
+        data: nameDetails[0]
+    })
 })
 
+app.get("/getAll", async (req, res) =>{
+    const j = await getNotes()
+    console.log(j)
+    res.status(200).json({ success: true,
+        data: j })
+})
+
+app.use((err, req, res, next ) => {
+    console.log(err.stack)
+    res.status(500).send('something went wrong')
+})
 app.listen(process.env.envPORT, 
     () => console.log(`listening ${process.env.envPORT}`))
--- server/dbService.js
+++ server/dbService.js
@@ -2,31 +2,57 @@
 const mysql = require('mysql2')
 require('dotenv').config()
 
-const connection = mysql.createConnection({
-    host: process.env.envHOST,
-    user: process.env.envUSER,
-    password: process.env.envPASSWORD,
-    database: process.env.envDATABASE,
-    port: process.env.DB_PORT
-})
-
-connection.connect((err) => {
-    if(err){
-        console.log(err.message)
-    }else{
-        console.log('db', connection.sqlMessage)
-    }
-})
-
-// const pool = mysql.createPool({
+// const connection = mysql.createConnection({
 //     host: process.env.envHOST,
 //     user: process.env.envUSER,
 //     password: process.env.envPASSWORD,
 //     database: process.env.envDATABASE,
-// }).promise()
+//     port: process.env.DB_PORT
+// })
 
-// const results = async () =>{
-//     const [result] = await pool.query("SELECT * FROM notes")
-//     console.log(result)
-// }
-// results()
+// connection.connect((err) => {
+//     if(err){
+//         console.log(err.message)
+//     }else{
+//         console.log('db', connection.sqlMessage)
+//     }
+// })
+
+const pool = mysql.createPool({
+    host: process.env.envHOST,
+    user: process.env.envUSER,
+    password: process.env.envPASSWORD,
+    database: process.env.envDATABASE,
+}).promise()
+
+const getNotes = async () =>{
+    const [result] = await pool.query("SELECT * FROM crud_app")
+    // console.log(result)
+    return result;
+}
+// getNotes()
+
+const getNote = async (id) =>{
+    const [result] = await pool.query(
+        `SELECT * FROM crud_app
+        where id = ?`, [id])
+    return result;
+}
+// getNote(4)
+
+const insertRow  = async(name, )=> {
+    const  date_added = new Date();
+
+    const [res] = await pool.query(`
+    INSERT INTO crud_app (name, date_added)
+    values(?, ?)`,[name, date_added])
+    // console.log(res.insertId)
+    return res.insertId;
+}
+// insertRow('test2', 'test insert')
+
+module.exports = {
+    getNote,
+    getNotes,
+    insertRow,
+}
